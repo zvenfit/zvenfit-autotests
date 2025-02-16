84341799774a4b6045d7a5541417a37bff125c6c
mr.alex <alexander.berlov@gmail.com>
2025-02-16 16:53:43 +0300

    upgrade

--- pages/contact_page.py
+++ pages/contact_page.py
@@ -4,7 +4,7 @@ class ContactPage(BasePage):
     def __init__(self, page):
         super().__init__(page)
         self.name_input = "#ooalnvy > div.sb-container > form > div:nth-child(3) > div > div.sb-input__wrapper.s-form-type-1__input-wrapper > label"
-        self.number_input = "#ooalnvy\/1"
+        self.number_input = "#ooalnvy > div.sb-container > form > div:nth-child(4) > div > div.sb-input__wrapper.s-form-type-1__input-wrapper > label"
         self.submit_button = "#ooalnvy > div.sb-container > form > div.sb-col_lg-6.sb-col_md-6.sb-col_sm-12.sb-col_xs-12.s-form-type-1__submit > input"
         self.success_message = "#ooalnvy > div.sb-container > form > div.sb-col_lg-6.sb-col_md-6.sb-col_sm-12.sb-col_xs-12.s-form-type-1__submit > div.sb-notification.sb-notification_success.sb-m-17-top.sb-font-p3"
 
